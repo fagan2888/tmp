0c7374d9f9ed98df4593f57ec5cbc81ce3b98c8e
zhaoliyuan <zhaoliyuan@licaimofang.com>
2019-10-21 13:58:17 +0800

    add analysis

--- shell/analysis.py
+++ shell/analysis.py
@@ -8,8 +8,65 @@ import pandas as pd
 from datetime import datetime
 from ipdb import set_trace
 
-#df = pd.read_excel('output.xlsx',sheet_name = 0)
-df = pd.read_excel('output.xlsx',sheet_name = 1)
+#df = pd.read_excel('output.xlsx',index_col = 0, sheet_name = 0)
+#df0 = df.copy()
+#df = pd.read_excel('output.xlsx',sheet_name = 1)
+#df1 = df.copy()
+#
+## make a new dataframe
+#df0.columns = ['uid','age','asset','expenditure','created_at']
+#df0.drop(['uid','expenditure','created_at'], inplace = True, axis = 1)
+#df1.drop('uid', inplace = True, axis = 1)
+## firstly use mean value to calculate see people in the same age as one single person
+#df0 = df0.groupby('age').mean()
+#df0.reset_index(inplace = True)
+#df1 = df1.groupby('age').mean()
+#df1.reset_index(inplace = True)
+
+def ratio(df, step, column = 'age'):
+    df.sort_values(column,inplace = True, ascending = True)
+    ratio = list()
+    for i in range(len(df)):
+        col = df[column][i]
+        allExpenditure = df[df[column] >= col]['expenditure'].sum()
+        dfE = df[df[column] >= col]
+        dfE = dfE[dfE[column] < (col + step)]
+        expenditure = dfE['expenditure'].sum()
+        ratio.append(expenditure / allExpenditure)
+
+    return ratio
+
+#ratioOneYear = ratio(df1, step = 1)
+#ratioTwoYear = ratio(df1, step = 2)
+#ratioThreeYear = ratio(df1, step = 3)
+#ratioFourYear = ratio(df1, step = 4)
+#ratioFiveYear = ratio(df1, step = 5)
+#ratioSixYear = ratio(df1, step = 6)
+#ratioSevenYear = ratio(df1, step = 7)
+#ratioEightYear = ratio(df1, step = 8)
+#ratioNineYear = ratio(df1, step = 9)
+#ratioTenYear = ratio(df1, step = 10)
+#
+#df0.sort_values('age', inplace = True, ascending = True)
+#df1.sort_values('age', inplace = True, ascending = True)
+#df0.set_index('age', inplace = True)
+#df1.set_index('age', inplace = True)
+#newDf = pd.merge(df0, df1, left_index = True, right_index = True)
+#expenditure = newDf['expenditure']
+#newDf.drop('expenditure', axis = 1, inplace = True)
+#newDf['expenditure'] = expenditure
+#newDf['ratioOneYear'] = ratioOneYear
+#newDf['ratioTwoYear'] = ratioTwoYear
+#newDf['ratioThreeYear'] = ratioThreeYear
+#newDf['ratioFourYear'] = ratioFourYear 
+#newDf['ratioFiveYear'] = ratioFiveYear 
+#newDf['ratioSixYear'] = ratioSixYear
+#newDf['ratioSevenYear'] = ratioSevenYear
+#newDf['ratioEightYear'] = ratioEightYear
+#newDf['ratioNineYear'] = ratioNiincomeneYear 
+#newDf['ratioTenYear'] = ratioTenYear
+#
+#newDf.to_excel('analysisGrouped.xlsx')
 
 ## analysis relation between expenditure and age
 #age_expenditure = pd.DataFrame()
@@ -111,48 +168,51 @@ def group (df, column, step):
 #excel.save()
 
 
-# analysis relation between expenditure and income asset
-ie = df.loc[:,['income','expenditure']]
-
-ie30 = df[df['age']<30]
-ie30 = ie30.loc[:,['income','expenditure']]
-
-ie40 = df[df['age']>=30]
-ie40 = ie40[ie40['age']<40]
-ie40 = ie40.loc[:,['income','expenditure']]
-
-ie50 = df[df['age']>=40]
-ie50 = ie50[ie50['age']<50]
-ie50 = ie50.loc[:,['income','expenditure']]
-
-ie60 = df[df['age']>=50]
-ie60 = ie60[ie60['age']<60]
-ie60 = ie60.loc[:,['income','expenditure']]
-
-ie70 = df[df['age']>=60]
-ie70 = ie70[ie70['age']<70]
-ie70 = ie70.loc[:,['income','expenditure']]
-
-ie86 = df[df['age']>=70]
-ie86 = ie86[ie86['age']<86]
-ie86 = ie86.loc[:,['income','expenditure']]
-
-ie  = group(ie, 'income', 100000).groupby('group').mean()
-ie30  = group(ie30, 'income', 100000).groupby('group').mean()
-ie40  = group(ie40, 'income', 100000).groupby('group').mean()
-ie50  = group(ie50, 'income', 100000).groupby('group').mean()
-ie60  = group(ie60, 'income', 100000).groupby('group').mean()
-ie70  = group(ie70, 'income', 100000).groupby('group').mean()
-ie86  = group(ie86, 'income', 100000).groupby('group').mean()
-
-excel = pd.ExcelWriter('incomeExpenditureGrouped.xlsx')
-ie.to_excel(excel, 'all')
-ie30.to_excel(excel, '30')
-ie40.to_excel(excel, '40')
-ie50.to_excel(excel, '50')
-ie60.to_excel(excel, '60')
-ie70.to_excel(excel, '70')
-ie86.to_excel(excel, '86')
-excel.save()
-
-
+## analysis relation between expenditure and income asset
+#ie = df.loc[:,['income','expenditure']]
+#
+#ie30 = df[df['age']<30]
+#ie30 = ie30.loc[:,['income','expenditure']]
+#
+#ie40 = df[df['age']>=30]
+#ie40 = ie40[ie40['age']<40]
+#ie40 = ie40.loc[:,['income','expenditure']]
+#
+#ie50 = df[df['age']>=40]
+#ie50 = ie50[ie50['age']<50]
+#ie50 = ie50.loc[:,['income','expenditure']]
+#
+#ie60 = df[df['age']>=50]
+#ie60 = ie60[ie60['age']<60]
+#ie60 = ie60.loc[:,['income','expenditure']]
+#
+#ie70 = df[df['age']>=60]
+#ie70 = ie70[ie70['age']<70]
+#ie70 = ie70.loc[:,['income','expenditure']]
+#
+#ie86 = df[df['age']>=70]
+#ie86 = ie86[ie86['age']<86]
+#ie86 = ie86.loc[:,['income','expenditure']]
+#
+#ie  = group(ie, 'income', 100000).groupby('group').mean()
+#ie30  = group(ie30, 'income', 100000).groupby('group').mean()
+#ie40  = group(ie40, 'income', 100000).groupby('group').mean()
+#ie50  = group(ie50, 'income', 100000).groupby('group').mean()
+#ie60  = group(ie60, 'income', 100000).groupby('group').mean()
+#ie70  = group(ie70, 'income', 100000).groupby('group').mean()
+#ie86  = group(ie86, 'income', 100000).groupby('group').mean()
+#
+#excel = pd.ExcelWriter('incomeExpenditureGrouped.xlsx')
+#ie.to_excel(excel, 'all')
+#ie30.to_excel(excel, '30')
+#ie40.to_excel(excel, '40')
+#ie50.to_excel(excel, '50')
+#ie60.to_excel(excel, '60')
+#ie70.to_excel(excel, '70')
+#ie86.to_excel(excel, '86')
+#excel.save()
+#
+# analysis ratio using grouped income
+#df = pd.read_excel('incomeExpenditureGrouped.xlsx')
+df = pd.read_excel('analysis.xlsx')
+print(df[df['age'] <= 18])
