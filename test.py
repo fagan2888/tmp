5db074bec2c9c04056161f0efe662766ef797f0b
zhaoliyuan <zhaoliyuan@licaimofang.com>
2019-12-16 11:59:23 +0800

    new

--- test.py
+++ test.py
@@ -21,20 +21,21 @@ from multiprocessing import Process
 #    a[i] = i
 #print(a)
 #
-test = '\\'
-print(test)
+def test():
+    test = '\\'
+    print(test)
 
-print(ord('+'))
-print(ord('＋'))
-print(ord('-'))
-print(ord('－'))
+    print(ord('+'))
+    print(ord('＋'))
+    print(ord('-'))
+    print(ord('－'))
 
-a = pd.DataFrame({'a':[1,2,3], 'b':[3,4,5], 'c':[4,5,6], 'd':[5,6,7]})
-print(a)
-b = a.set_index(['a','c']).unstack()['b']
-print(b)
-c = a.set_index('b').unstack()
-print(c)
+    a = pd.DataFrame({'a':[1,2,3], 'b':[3,4,5], 'c':[4,5,6], 'd':[5,6,7]})
+    print(a)
+    b = a.set_index(['a','c']).unstack()['b']
+    print(b)
+    c = a.set_index('b').unstack()
+    print(c)
 
 def test(a):
     print('hello world', a)
@@ -43,7 +44,25 @@ def test2():
     print('hello there')
 
 if __name__ == '__main__':
-    p1 = Process(target = test, args = (1,))
-    p2 = Process(target = test2)
-    p1.start()
-    p2.start()
+#    p1 = Process(target = test, args = (1,))
+#    p2 = Process(target = test2)
+#    p1.start()
+#    p2.start()
+    df = pd.DataFrame({'a':[1,1,2,2,3,4,5],'b':[1,2,2,3,4,2,6]})
+    df['c'] = 1
+    print(df) 
+#    dfg = df.groupby('a')
+#    dfg.get_group(2).loc['c'] = 0
+#    dfg.get_group(1).loc['c'] = 1
+#    dfg.get_group(3).loc['c'] = 2
+#    print(dfg.get_group(1))
+#    df1 = dfg.get_group(1)
+#    df2 = dfg.get_group(2)
+#    df3 = dfg.get_group(3)
+#    df = pd.concat([df1,df2,df3], axis = 0)
+#    print(df)
+    dfa = df.pivot(index = 'a', columns = 'b', values = 'c').fillna(0)
+    print(dfa)
+
+
+
